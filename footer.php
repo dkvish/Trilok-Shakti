b936f98a515d1fe1994435edab738f932a4eeb81
Deepak Vishwakarma <dkvish.in@gmail.com>
2023-05-26 15:25:30 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="container-1 bg-orange">
+    <div class="row">
+        <div class="col-lg-6 text-white">
+            <h2 style="text-align:center"><i class="bi bi-building-fill"></i><b> कार्यालय पता</h2></b>
+            <div class="hr-line"></div>
+            <div style="font-size:1.2rem">
+                <i class="bi bi-geo-alt-fill"></i> पंडा टोली , रुद्रपुर, देवरिया, उ०प्र०<br>
+                <i class="bi bi-telephone-fill"></i> +91 9935264858<br>
+                <i class="bi bi-envelope-at-fill"></i> info@trilokshakti.com<br>
+                <i class="bi bi-globe-central-south-asia"></i> www.trilokshakti.com
+            </div>
+        </div>
+        <div class="col-lg-6 text-white">
+            <h2 style="text-align:center"><i class="bi bi-envelope-open-fill"></i><b> संपर्क करें</h2></b>
+            <div class="hr-line"></div>
+            <div class="row form-group">
+                <div class="col-lg-6" style="margin-bottom:1rem;">
+                    <input type="text" class="form-control" placeholder="Full Name">
+                </div>
+                <div class="col-lg-6" style="margin-bottom:1rem;">
+                    <input type="email" class="form-control" placeholder="E-Mail">
+                </div>
+            </div>
+            <div class="row form-group"style="margin-bottom:1rem">
+                <div class="col-lg-9"style="margin-bottom:1rem">
+                    <input type="text" class="form-control" placeholder="Message">
+                </div>
+                <div class="col-lg-3 form-group" style="text-align:center;">
+                    <button type="submit" class="btn btn-info">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>श्री त्रिलोक शक्ति</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+
+
+    
+
+    <script src="./js/bootstrap.bundle.js"></script>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-orange sticky-top">
+        <div class="container-fluid">
+                <a class="navbar-brand" href="#"><b>श्री त्रिलोक शक्ति</b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.php">मुख्यपृष्ठ</a>
+                    <a class="nav-link active" href="#notice">सूचना पृष्ठ</a>
+                    <a class="nav-link active" href="#info">संस्था परिचय</a>
+                    <a class="nav-link active" href="#members">प्रबंध-कार्यकारिणी</a>
+                    <a class="nav-link active" href="#gallery">फ़ोटो/विडिओ गॅलरी</a>
+                    <a class="nav-link active" href="#">संपर्क</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</head>
